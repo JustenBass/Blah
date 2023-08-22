be6a58be6fd4b4956be01f7c1ade3c2447c866fe
Justin Bass <justinbass6036@gmail.com>
2023-08-22 00:22:12 -0400

    Added RecordInvalid and authroize method in ApplicationController for all controllers that have a create actiion and recieves bad data. Added skip before action for authorize method on Userand Session Controllers because a user should not have to be authorized to sign in or sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :raise_invalid_exception
+  before_action :authorize
+
+
+  private
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { error: ["Not authorized"] }, status: :unauthorized unless @current_user
+end
+
+def raise_invalid_exception(invalid)
+  render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+    
     def index
         render json: User.all
     end
+
+
+    #signup
+    def create
+        user = User.create!(user_params)
+            session[:user_id] = user.id
+            render json: user
+    end
+
+    #stay logged in
+    def show
+       render json: @current_user
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
 end
