2d977e6d861aa7243bdc26c29eeb9be5dd7a26b4
Justin Bass <justinbass6036@gmail.com>
2023-09-04 22:15:08 -0400

    Removed current_user private method in Comments controller because I set a set in instance variable with a value of the current user in ApllocationController allowing me to use this variable in any instance method in other controllers.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-    
+
     def create
         comment = @current_user.comments.create!(comment_params)
         render json: comment, status: :created
@@ -22,8 +22,5 @@ class CommentsController < ApplicationController
     def comment_params
         params.permit(:comment, :blog_id)
     end
-
-    # def current_user
-    #     @current_user = User.find_by(id: session[:user_id])
-    # end
+    
 end
